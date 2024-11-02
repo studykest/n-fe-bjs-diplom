8e48df9a17b00963383e39d910dc857b1976c83b
studykest <studyke@yandex.ru>
2024-11-02 17:29:13 +0300

    реализованны функции для операций с деньгами

--- public/homePage.js
+++ public/homePage.js
@@ -36,3 +36,29 @@ function showRatesBoard() {
 }
 
 setInterval(showRatesBoard, 1000);
+
+const moneyManager = new MoneyManager();
+
+function checkResponseMoneyManager(response, message) {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+    moneyManager.setMessage(response.success, message);
+  } else {
+    moneyManager.setMessage(response.success, response.error);
+  }
+}
+
+moneyManager.addMoneyCallback = (data) => {
+  let message = `Счёт пополнен на сумму: ${data.amount} ${data.currency}`;
+  ApiConnector.addMoney(data, response => checkResponseMoneyManager(response, message));
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  let message = `Конвертация валют прошла успешно: ${data.fromAmount} ${data.fromCurrency} в ${data.targetCurrency}`;
+  ApiConnector.convertMoney(data, response => checkResponseMoneyManager(response, message));
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+  let message = `Перевод валют прошла успешно: ${data.amount} ${data.currency}`;
+  ApiConnector.transferMoney(data, response => checkResponseMoneyManager(response, message));
+};
