59905d0e34c85ec187186eb69538093054aaa821
studykest <studyke@yandex.ru>
2024-11-02 23:26:14 +0300

    реализованы функции для работы с избранным

--- public/homePage.js
+++ public/homePage.js
@@ -49,16 +49,40 @@ function checkResponseMoneyManager(response, message) {
 }
 
 moneyManager.addMoneyCallback = (data) => {
-  let message = `Счёт пополнен на сумму: ${data.amount} ${data.currency}`;
+  let message = `Счёт пополнен: ${data.amount} ${data.currency}`;
   ApiConnector.addMoney(data, response => checkResponseMoneyManager(response, message));
 };
 
 moneyManager.conversionMoneyCallback = (data) => {
-  let message = `Конвертация валют прошла успешно: ${data.fromAmount} ${data.fromCurrency} в ${data.targetCurrency}`;
+  let message = `Конвертация валюты: ${data.fromAmount} ${data.fromCurrency} в ${data.targetCurrency}`;
   ApiConnector.convertMoney(data, response => checkResponseMoneyManager(response, message));
 };
 
 moneyManager.sendMoneyCallback = (data) => {
-  let message = `Перевод валют прошла успешно: ${data.amount} ${data.currency}`;
+  let message = `Перевод валюты: ${data.amount} ${data.currency}`;
   ApiConnector.transferMoney(data, response => checkResponseMoneyManager(response, message));
 };
+
+const favoritesWidget = new FavoritesWidget();
+
+function checkResponse2(response) {
+  if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  } else {
+    response.error;
+  }
+}
+
+ApiConnector.getFavorites(checkResponse2);
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, checkResponse2);
+  favoritesWidget.setMessage(data.id, 'Добавлен новый пользователь');
+}
+
+favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, checkResponse2);
+  favoritesWidget.setMessage(data.id, 'Удалён пользователь');
+}
